aa8c32a1769d0a040d5dd19000c9f19022cf52fc
MrTitmouse <150177605+MrTitmouse@users.noreply.github.com>
2023-11-07 21:34:18 +0100

    Update detailsDisplayer.js

--- detailsDisplayer.js
+++ detailsDisplayer.js
@@ -2,7 +2,6 @@ function showProductDetails(name, kategoria, id, opis, cena_netto) {
     productInfo.innerHTML = `
         <h3>${name}</h3>
         <p><strong>kategoria:</strong> ${kategoria}</p>
-        <p><strong>opis:</strong> ${opis}</p>
         <p><strong>id:</strong> ${id}</p>
         <p><strong>cena_netto:</strong> ${cena_netto}</p>
     `;
