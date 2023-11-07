52f1d2db542585a26f8f5590e2a3f2a8678e4012
MrTitmouse <150177605+MrTitmouse@users.noreply.github.com>
2023-11-07 21:18:36 +0100

    Update detailsDisplayer.js

--- detailsDisplayer.js
+++ detailsDisplayer.js
@@ -1,8 +1,10 @@
 function showProductDetails(name, category, description) {
     productInfo.innerHTML = `
         <h3>${name}</h3>
-        <p><strong>Category:</strong> ${category}</p>
-        <p><strong>Description:</strong> ${description}</p>
+        <p><strong>Kategoria:</strong> ${kategoria}</p>
+        <p><strong>Opis:</strong> ${opis}</p>
+        <p><strong>Id:</strong> ${id}</p>
+        <p><strong>Cena:</strong> ${cena_netto}</p>
     `;
 
     productDetails.style.display = 'block';
