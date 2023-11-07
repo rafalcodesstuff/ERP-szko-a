88d233a4a20c960c5bd4923e14628c00d9d19627
MrTitmouse <150177605+MrTitmouse@users.noreply.github.com>
2023-11-07 21:39:28 +0100

    Update searchProducts.js

--- searchProducts.js
+++ searchProducts.js
@@ -2,13 +2,13 @@
 const products = [
     { nazwa: "Żarówka LED Philips 10W E27", kategoria: "Żarówki LED", id:"159789", cena_netto:"12,00zł" },
     { nazwa: "Pasek LED RGB 2m", kategoria: "Paski LED", id:"587963", cena_netto:"105,70zł" },
-    { nazwa: "Oprawa żarówki E14", kategoria: "Oprawy", id:"100987", cena_netto:"5,80" },
-    { nazwa: "Kinkiet Classic", kategoria: "Kinkiety", id:"189456", cena_netto:"250,00" },
-    { nazwa: "Lampa zewnętrzna z czuknikiem ruchu", kategoria: "Oświetlenie zewnętrzne", id:"859674", cena_netto:"136,70" },
-    { nazwa: "Żarówka przemysłowa Warta 60W", kategoria: "Żarówki klasyczne", id:"185444", cena_netto:"3,50" },
-    { nazwa: "Lampa stołowa Modren", kategoria: "Lampy stołowe", id:"033358", cena_netto:"340,00" },
-    { nazwa: "Lampka na biurko", kategoria: "Biuro", id:"213755", cena_netto:"53,50" },
-    { nazwa: "Żarówka LED Lethe 8W", kategoria: "Żarówki LED", id:"147563", cena_netto:"8,99" },
+    { nazwa: "Oprawa żarówki E14", kategoria: "Oprawy", id:"100987", cena_netto:"5,80zł" },
+    { nazwa: "Kinkiet Classic", kategoria: "Kinkiety", id:"189456", cena_netto:"250,00zł" },
+    { nazwa: "Lampa zewnętrzna z czuknikiem ruchu", kategoria: "Oświetlenie zewnętrzne", id:"859674", cena_netto:"136,70zł" },
+    { nazwa: "Żarówka przemysłowa Warta 60W", kategoria: "Żarówki klasyczne", id:"185444", cena_netto:"3,50zł" },
+    { nazwa: "Lampa stołowa Modren", kategoria: "Lampy stołowe", id:"033358", cena_netto:"340,00zł" },
+    { nazwa: "Lampka na biurko", kategoria: "Biuro", id:"213755", cena_netto:"53,50zł" },
+    { nazwa: "Żarówka LED Lethe 8W", kategoria: "Żarówki LED", id:"147563", cena_netto:"8,99zł" },
     // Add more products as needed
 ];
 
